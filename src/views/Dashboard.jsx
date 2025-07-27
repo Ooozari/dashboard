8455873b4199fcfc180b10f4c9aa73187c0a6f90
Ooozari <uzairasiff1227@gmail.com>
2025-07-27 20:52:00 +0500

    reponsive for mobile

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -1,91 +1,79 @@
-import LeftNavigation from '@/components/shared/LeftNavigation'
-import MyTask from '@/components/shared/MyTask'
-import DonutChart from '@/components/shared/DonutChart'
-import MyMeetings from '@/components/shared/MyMeetings'
-import React from 'react'
-import InvoiceOverview from '@/components/shared/InvoiceOverview'
-import TopNavBar from '@/layouts/TopNavBar'
-import { Input } from "@/components/ui/input"
-import IncomeVsExpenseChart from '@/components/shared/IncomeVsExpenseChart'
-
+"use client";
+import React from "react";
+import LeftNavigation from "@/components/shared/LeftNavigation";
+import MyTask from "@/components/shared/MyTask";
+import DonutChart from "@/components/shared/DonutChart";
+import MyMeetings from "@/components/shared/MyMeetings";
+import InvoiceOverview from "@/components/shared/InvoiceOverview";
+import TopNavBar from "@/layouts/TopNavBar";
+import { Input } from "@/components/ui/input";
+import IncomeVsExpenseChart from "@/components/shared/IncomeVsExpenseChart";
 
 function Dashboard() {
-    return (
-        <>
+  return (
+    <div className="bg-blue-50 min-h-screen">
+      {/* Top Navbar */}
+      <TopNavBar />
+
+      <div className="flex flex-col lg:flex-row w-full">
+        {/* Left Navigation - hidden on mobile */}
+        <div className="hidden lg:block">
+          <LeftNavigation />
+        </div>
+
+        {/* Main Content */}
+        <div className="flex-1 px-4 py-5 w-full">
+          {/* Header with search bar */}
+          <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between pb-6 gap-2">
+            {/* Heading */}
             <div>
-                <div className='bg-blue-50  w-full px-2'>
-                    {/* Top Navbar */}
-                    <div>
-                        <TopNavBar />
-                    </div>
+              <p className="text-Black font-medium leading-3 text-sm sm:text-base">
+                Manage and track your projects
+              </p>
+              <h1 className="text-2xl sm:text-3xl font-extrabold text-Black">
+                Project Dashboard
+              </h1>
+            </div>
+            {/* Search bar */}
+            <div className="w-full sm:w-1/2">
+              <Input
+                placeholder="Search tasks, meetings, projects..."
+                className="rounded-full w-full"
+              />
+            </div>
+          </div>
 
-                    <div className='flex w-full py-5'>
-                        {/* Left Navigation */}
-                        <div>
-                            <LeftNavigation />
-                        </div>
-
-                        {/* dashboard content */}
-                        <div className='w-full px-4 '>
-
-                            {/* Header with search bar */}
-                            <div className='flex justify-between pb-6 '>
-                                {/* Heading */}
-                                <div>
-                                    <p className='text-Black font-md leading-3'>Manage and track your projects</p>
-                                    <h1 className='text-3xl font-extrabold text-Black'>Project Dashboard</h1>
-
-                                </div>
-                                {/* Search bar */}
-                                <div className='w-1/3'>
-                                    <Input placeholder='Search tasks, meetings, projects..' className='rounded-full w-full' />
-                                </div>
-
-                            </div>
-
-                            <div className='flex gap-1'>
-                                {/*LEFT --- My tasks */}
-                                <div className='pr-2'>
-                                    <MyTask />
-                                </div>
-                                {/* MIDDLE -graphs + Invoices bars */}
-                                <div className='w-full flex flex-col gap-1'>
-                                    <div className='flex gap-1 w-full'>
-                                        <div className='w-full'>
-                                            <DonutChart />
-                                        </div>
-                                        <div className='w-full'>
-                                            <IncomeVsExpenseChart />
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <InvoiceOverview />
-                                    </div>
-                                </div>
-                                {/*RIGHT --- My Meetings */}
-                                <div>
-                                    <MyMeetings />
-                                </div>
-                            </div>
-
-
-
-                        </div>
-
-
-                    </div>
-
-
-
-
-
-
-                </div>
+          {/* Dashboard Layout */}
+          <div className="flex flex-col xl:flex-row gap-4">
+            {/* My Tasks */}
+            <div className="xl:w-1/4">
+              <MyTask />
             </div>
 
+            {/* Charts and Invoice */}
+            <div className="flex-1 flex flex-col gap-4">
+              {/* Charts */}
+              <div className="flex flex-col md:flex-row gap-4">
+                <div className="w-full">
+                  <DonutChart />
+                </div>
+                <div className="w-full">
+                  <IncomeVsExpenseChart />
+                </div>
+              </div>
+              {/* Invoice */}
+              <InvoiceOverview />
+            </div>
 
-        </>
-    )
+            {/* My Meetings */}
+            <div className="xl:w-1/4">
+              <MyMeetings />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
